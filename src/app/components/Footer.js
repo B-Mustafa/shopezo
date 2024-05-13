00d0b2334750df4d9973cfeb8a1dcc47409b8c05
Mustafa <mustafabhikhpurwala@gmail.com>
2024-05-13 22:13:14 +0530

    didnt had time so just made 2 files footer and product card

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      Here Goes the Footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/app/components/ProductCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductCard() {
+  return (
+    <div>
+      Product Card
+    </div>
+  )
+}
+
+export default ProductCard
